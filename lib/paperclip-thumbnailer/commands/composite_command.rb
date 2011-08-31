6498db277fa6fc459fba4371f290fcc85903e846
Mike Burns <mburns@thoughtbot.com>
2011-08-31 11:03:04 -0400

    Tested CompositeCommand.

--- lib/paperclip-thumbnailer/commands/composite_command.rb
+++ lib/paperclip-thumbnailer/commands/composite_command.rb
@@ -1,59 +1,57 @@
-module Paperclip
-  module Thumbnailer
+module PaperclipThumbnailer
 
-    class CompositeCommand
-      def initialize
-        @flags = []
-        @options = {}
-      end
-
-      def tag
-        :composite
-      end
-
-      def for_command(tag)
-        self
-      end
-
-      def with_options(options)
-        @options = @options.merge(options)
-        self
-      end
-
-      def with_source(source)
-        @source = source
-        self
-      end
-
-      def with_destination(destination)
-        @destination = destination
-        self
-      end
-
-      def with_flag(arg, value = false)
-        unless @flags.include?("-#{arg}")
-          @flags << "-#{arg}"
-          @flags << %{"#{value}"} if value
-        end
-        self
-      end
-
-      def to_s
-        "composite " + [source_file_options, source, flags, convert_options, destination].flatten.join(' ')
-      end
-
-      protected
-
-      attr_reader :source, :destination, :flags
-
-      def source_file_options
-        @options[:source_file_options]
-      end
-
-      def convert_options
-        @options[:convert_options]
-      end
+  class CompositeCommand
+    def initialize
+      @flags = []
+      @options = {}
     end
 
+    def tag
+      :composite
+    end
+
+    def for_command(tag)
+      self
+    end
+
+    def with_options(options)
+      @options = @options.merge(options)
+      self
+    end
+
+    def with_source(source)
+      @source = source
+      self
+    end
+
+    def with_destination(destination)
+      @destination = destination
+      self
+    end
+
+    def with_flag(arg, value = false)
+      unless @flags.include?("-#{arg}")
+        @flags << "-#{arg}"
+        @flags << %{"#{value}"} if value
+      end
+      self
+    end
+
+    def to_s
+      "composite " + [source_file_options, source, flags, destination_file_options, destination].flatten.join(' ')
+    end
+
+    protected
+
+    attr_reader :source, :destination, :flags
+
+    def source_file_options
+      @options[:source_file_options]
+    end
+
+    def destination_file_options
+      @options[:destination_file_options]
+    end
   end
+
 end
---
+++ spec/commands/composite_command_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+require 'paperclip-thumbnailer/commands/composite_command'
+
+describe PaperclipThumbnailer::CompositeCommand do
+  subject { PaperclipThumbnailer::CompositeCommand.new }
+  let(:tag) { :composite }
+
+  it "has the appropriate tag" do
+    subject.tag.should == tag
+  end
+
+  it "understands #for_command" do
+    subject.for_command(:foo).should respond_to(:for_command)
+  end
+end
+
+describe PaperclipThumbnailer::CompositeCommand, 'builder' do
+  subject { PaperclipThumbnailer::CompositeCommand.new }
+  let(:source) { 'source' }
+  let(:destination) { 'destination' }
+  let(:expected_command) {
+    %{composite a b c #{source} -a "b" -c d e f #{destination}}
+  }
+
+  before do
+    subject.
+      with_options(:source_file_options => %w(a b c)).
+      with_options(:destination_file_options => %w(d e f)).
+      with_source(source).
+      with_destination(destination).
+      with_flag(:a, 'b').
+      with_flag(:c).
+      with_flag(:a, 'd')
+  end
+
+  it "should produce the expected command" do
+    subject.to_s.should == expected_command
+  end
+end
+
