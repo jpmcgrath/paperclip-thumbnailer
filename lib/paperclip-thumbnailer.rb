d9634402f579b513b31404730956f39c41dc6cde
Mike Burns <mburns@thoughtbot.com>
2011-08-31 11:36:44 -0400

    Hook into Paperclip.

--- lib/paperclip-thumbnailer.rb
+++ lib/paperclip-thumbnailer.rb
@@ -1,1 +1,15 @@
-require 'paperclip-thumbnailer/processors/thumbnail'
+require 'paperclip-thumbnailer/processor'
+require 'paperclip-thumbnailer/filters/thumbnailer_filter'
+require 'paperclip-thumbnailer/filters/filter_terminus'
+require 'paperclip-thumbnailer/commands/convert_command'
+require 'paperclip'
+
+module PaperclipThumbnailer
+  ::Paperclip.configure do |c|
+    c.register_processor :basic_thumbnailer,
+      Processor.build_from(
+        [ThumbnailerFilter.new,
+          FilterTerminus.new(ConvertCommand.new)]
+    )
+  end
+end
